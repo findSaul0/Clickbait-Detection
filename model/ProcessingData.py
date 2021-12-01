490bcbab48214532a790894e6ba46818c95d8bd9
peppe-99 <g.cardaropoli99@gmail.com>
2021-12-01 20:15:07 +0100

    Script for data visualization

---
+++ model/ProcessingData.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, GlobalMaxPooling1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+if __name__ == '__main__':
+    # Carichiamo l'intero dataset
+    DATASET_PATH = "../data/clickbait_data.csv"
+    data = pd.read_csv(DATASET_PATH)
+
+    # otteniamo i titoli e le etichette
+    headlines = data['headline'].values
+    labels = data['clickbait'].values
+
+    # Mostriamo la ripartizione
+    sns.set_theme(style="darkgrid")
+    ax = sns.countplot(x="clickbait", data=data)
+    plt.show()
+
+
+    # dividiamo i dati di training e testing
+    headlines_train, headlines_test, labels_train, labels_test = train_test_split(headlines, labels)
+    print(f"Campioni di training: {headlines_train.shape}\nCampioni di testing: {headlines_train.shape}")
