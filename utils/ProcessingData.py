6c4ee911f6736d44d972017ad919cef8214563bb
peppe-99 <g.cardaropoli99@gmail.com>
2021-12-02 13:57:38 +0100

    change functions

--- utils/ProcessingData.py
+++ utils/ProcessingData.py
@@ -19,31 +19,53 @@ def load_dataset(path):
 
 # Funzione per creare un grafico a barre che mostra
 # la ripatizione in classi dei campioni
-def show_datalet(data):
-    sns.set_theme(style="darkgrid")
-    ax = sns.countplot(x="clickbait", data=data)
-    plt.savefig("../log/data_classes.png")
+def show_data_classes(data):
+    sns.set_style('darkgrid')
+    plt.figure(figsize=(7, 5))
+    fig1 = sns.countplot(data['clickbait'])
+    plt.title('Clickbait vs Non-Clickbait')
+    plt.ylabel('# of Headline')
+    plt.xlabel('Type of Headline')
+    fig1.set(xticklabels=['Non-Clickbait', 'Clickbait'])
+    plt.tight_layout()
+    #p lt.savefig("../log/data_classes.png")
+
+def show_most_frequency_clickbait_word(words, counts):
+    plt.style.use('seaborn-talk')
+    word_freq_figure1 = plt.figure(figsize=(10, 6))
+    sns.barplot(words, counts, palette='Oranges_d')
+    plt.xticks(fontsize=16)
+    plt.xticks(rotation=80)
+    plt.title('Top 20 Clickbait Headline Words')
+    plt.xlabel('Most Common Words')
+    plt.ylabel('Word Count')
+    sns.set_style('darkgrid')
+    plt.savefig('../log/clickbait_20_bar')
+
+def show_most_frequency_nonclickbait_word(words, counts):
+    word_freq_figure1 = plt.figure(figsize=(10, 6))
+    sns.barplot(words, counts, palette='Blues_d')
+    plt.xticks(fontsize=16)
+    plt.xticks(rotation=80)
+    plt.title('Top 20 Non-Clickbait Headline Words')
+    plt.xlabel('Most Common Words')
+    plt.ylabel('Word Count')
+    sns.set_style('darkgrid')
+    plt.savefig('../log/nonclickbait_20_bar')
 
 
 # Funzione per creare una word cloud
-def create_word_cloud(data):
-    headlines = data['headline'].values
-    text = ""
-    for word in headlines:
-        text += word
-    wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
+def create_word_clouds(data1, data2):
+    wordcloud = WordCloud(colormap='Spectral').generate_from_frequencies(data1)
+    plt.figure(figsize=(10, 10), facecolor='k')
+    plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
-    plt.savefig("../log/word_cloud.png")
+    plt.tight_layout(pad=0)
+    #plt.savefig('../log/clickbait_wordcloud')
 
-
-if __name__ == '__main__':
-    data, headlines, labels = load_dataset("../data/clickbait_data.csv")
-
-    show_datalet(data)
-    create_word_cloud(data)
-
-    # dividiamo i dati di training e testing
-    headlines_train, headlines_test, labels_train, labels_test = train_test_split(headlines, labels)
-    print(f"Campioni di training: {headlines_train.shape}\nCampioni di testing: {headlines_test.shape}")
+    wordcloud = WordCloud(colormap='Pastel2').generate_from_frequencies(data2)
+    plt.figure(figsize=(10, 10), facecolor='k')
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    # plt.savefig('../log/nonclickbait_wordcloud')
