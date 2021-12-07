fa22fa7dcf0eaadb13cdf27dd4545d33a3934188
peppe-99 <g.cardaropoli99@gmail.com>
2021-12-07 22:26:50 +0100

    Add function for make distribution graphs

--- utils/ProcessingData.py
+++ utils/ProcessingData.py
@@ -28,7 +28,8 @@ def show_data_classes(data):
     plt.xlabel('Type of Headline')
     fig1.set(xticklabels=['Non-Clickbait', 'Clickbait'])
     plt.tight_layout()
-    #p lt.savefig("../log/data_classes.png")
+    # p lt.savefig("../log/data_classes.png")
+
 
 def show_most_frequency_clickbait_word(words, counts):
     plt.style.use('seaborn-talk')
@@ -41,6 +42,7 @@ def show_most_frequency_clickbait_word(words, counts):
     plt.ylabel('Word Count')
     sns.set_style('darkgrid')
     plt.savefig('../log/clickbait_20_bar')
+
 
 def show_most_frequency_nonclickbait_word(words, counts):
     word_freq_figure1 = plt.figure(figsize=(10, 6))
@@ -61,7 +63,7 @@ def create_word_clouds(data1, data2):
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
     plt.tight_layout(pad=0)
-    #plt.savefig('../log/clickbait_wordcloud')
+    # plt.savefig('../log/clickbait_wordcloud')
 
     wordcloud = WordCloud(colormap='Pastel2').generate_from_frequencies(data2)
     plt.figure(figsize=(10, 10), facecolor='k')
@@ -69,3 +71,42 @@ def create_word_clouds(data1, data2):
     plt.axis("off")
     plt.tight_layout(pad=0)
     # plt.savefig('../log/nonclickbait_wordcloud')
+
+
+def show_samples_distribution(data):
+    sns.set_style('darkgrid')
+
+    # Creiamo un grafico per la distribuzione dei campioni in base alla feature "question"
+    # Questo grafico mostrerà quanti titoli clickbait e non-clickbait sono domande o meno.
+    plot = data.groupby('question')['clickbait'].value_counts().unstack().plot.bar(rot=0)
+    plot.set_xlabel('Headline is a question?')
+    plot.set_ylabel('Num of Headlines')
+    plot.legend(title=None, labels=['Non-Clickbait', 'Clickbait'])
+    plot.set(xticklabels=['No', 'Yes'])
+    # plt.savefig('../log/question_distribution.png')
+
+    # Creiamo un grafico per la distribuzione dei campioni in base alla feature "starts_with_num"
+    # Questo grafico mostrerà quantit titoli clickbait e non-clickbait iniziano con un numero o meno
+    plot = data.groupby('starts_with_num')['clickbait'].value_counts().unstack().plot.bar(rot=0)
+    plot.set_xlabel('Headline starts with a number?')
+    plot.set_ylabel('Num of Headlines')
+    plot.legend(title=None, labels=['Non-Clickbait', 'Clickbait'])
+    plot.set(xticklabels=['No', 'Yes'])
+    # plt.savefig('../log/starts_with_number_distribution.png')
+
+    # Creiamo un grafico per la distribuzione dei campioni in base alla feature "exclamation"
+    # Questo grafico mostrerà quantit titoli clickbait e non-clickbait iniziano con un numero o meno
+    plot = data.groupby('exclamation')['clickbait'].value_counts().unstack().plot.bar(rot=0)
+    plot.set_xlabel('Headline contains exclamation mark?')
+    plot.set_ylabel('Num of Headlines')
+    plot.legend(title=None, labels=['Non-Clickbait', 'Clickbait'])
+    plot.set(xticklabels=['No', 'Yes'])
+    # plt.savefig('../log/exclamation_distribution.png')
+
+    # Creiamo un grafico per la distribuzione dei campioni in base alla feature "headline_words"
+    # Questo grafico mostrerà la distribuzione titoli clickbait e non-clickbait in base al numero di parole
+    plot = data.groupby('headline_words')['clickbait'].value_counts().unstack().plot.bar(rot=0)
+    plot.set_xlabel('Num of Words')
+    plot.set_ylabel('Num of Headlines')
+    plot.legend(title=None, labels=['Non-Clickbait', 'Clickbait'], loc='upper right')
+    # plt.savefig('../log/headline_words_distribution.png')
